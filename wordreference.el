ea403db488bc2ecd2dc1dd1b3ee96eec5a1a1da3
mousebot <mousebot@riseup.net>
2022-03-16 11:25:54 +0100

    plists for result element, regexes to clean up printing

--- wordreference.el
+++ wordreference.el
@@ -40,10 +40,32 @@
 (defun wordreference--build-tds-text-list (tr)
   "Returns a list of results of both source and target langs."
   (let ((tds (dom-by-tag tr 'td)))
+    (setq wr-single-td (caddr tds))
     (mapcar (lambda (x)
-              ;; (if (not (dom-by-class x "ToWrd"))
-              (split-string (dom-texts x) "  " t " "))
+              (wr-build-single-td-list x))
             tds)))
+
+(defun wr-build-single-td-list (td)
+  ""
+  (cond ((or (dom-by-class td "ToWrd")
+             (dom-by-class td "FrWrd"))
+         (let* ((to-or-from (if (dom-by-class td "ToWrd")
+                                :to
+                              :from))
+                (em (dom-by-class td "tooltip POS2"))
+                (pos (dom-text em))
+                (tooltip (dom-by-tag em 'span))
+                (tooltip-text (dom-texts tooltip))
+                (term-text
+                 (if (dom-by-class td "FrWrd")
+                     (dom-text (dom-by-tag td 'strong))
+                   (dom-text td))))
+           `(,to-or-from ,term-text :pos ,pos :tooltip ,tooltip-text)))
+        ((or (dom-by-class td "ToEx")
+             (dom-by-class td "FrEx"))
+         `(:example ,(dom-texts td)))
+        (t
+         `(:other ,(dom-texts td)))))
 
 (defun wr-collect-trs-results-list (trs)
   ""
@@ -66,6 +88,11 @@
            (sup-trs (wordreference--get-trs sup-trs-table))
            (sup-trs-results-list (wr-collect-trs-results-list sup-trs)))
       ;; (setq wr-word-tables word-tables)
+      ;; (setq wr-full-html html-parsed)
+      ;; (setq wr-full-tables word-tables)
+      ;; (setq wr-single-tr (caddr (wordreference--get-trs pr-table)))
+      ;; (setq wr-full-pr-trs-list pr-trs-results-list)
+      ;; (setq wr-full-sup-trs-list sup-trs-results-list)
       (erase-buffer)
       (special-mode)
       (switch-to-buffer-other-window (current-buffer))
@@ -80,21 +107,34 @@
                            wordreference-source-lang
                            wordreference-target-lang)
                    'face font-lock-comment-face))
+      (wr-prop-query-in-results word)
       (goto-char (point-min)))))
 
 
 ;; PRINTING:
 
+(defun wr-prop-query-in-results (word)
+  "Add"
+  (let ((word-spl (split-string word)))
+    (save-excursion
+      (goto-char (point-min))
+      (mapc (lambda (x)
+              (while (search-forward-regexp (concat "\\b" x "\\b")
+                                            nil 'noerror)
+                (add-text-properties (- (point) (length x)) (point)
+                                     '(face '((t :inherit success :weight bold)))))
+              (goto-char (point-min)))
+            word-spl))))
+
 (defun wr-print-heading (heading)
   ""
   (insert (propertize heading
                       'face '((t :inherit font-lock-function-name-face
-                                 :weight bold)))
-          "\n\n"))
+                                 :weight bold)))))
 
 (defun wr-print-trs-results (trs)
   ""
-  (let* ((table-name (caaar trs))
+  (let* ((table-name (plist-get (caar trs) :other))
          (langs (cadr trs))
          (source (caar langs))
          (target (caaddr langs))
@@ -109,132 +149,106 @@
   ""
   (mapcar (lambda (def)
             (wr-print-single-definition def))
-          defs))
+          defs)
+  (insert "\n"))
 
-(defun wr-get-pos-from-entry (entry)
-  ""
-  ;FIXME: doesn't work if we have a phrasal expression!
-  (when (< 1 (length entry))
-    (let ((pos (cadr entry)))
-      (car (split-string pos)))))
+(defun wr--cull-double-spaces (result)
+  "Remove any double spaces from RESULT."
+  (save-match-data
+    (while (string-match "[[:blank:]]\\{2\\}"
+                         result)
+      (setq result (replace-match
+                    " "
+                    t nil result))))
+  result)
+
+(defun wr--cull-single-spaces-in-brackets (result)
+  "Remove any spaces inside brackets from RESULT."
+  (save-match-data
+    (while (string-match
+            ;; ( + SPC + any number of chars + SPC + ):
+            "([[:blank:]]\\(.*\\)[[:blank:]])"
+            result)
+      (setq result (replace-match
+                    "(\\1)"
+                    t nil result))))
+  result)
+
+(defun wr--cull-space-between-brackets (result)
+  "Remove any spaces between closing brackets from RESULT."
+  (save-match-data
+    (while (string-match
+            ;; ] + SPC + ) :
+            "][[:blank:]])"
+            result)
+      (setq result (replace-match
+                    "])"
+                    t nil result))))
+  result)
 
 (defun wr-print-single-definition (def)
-  ""
+  "Print a single definition DEF in the buffer.
+\nFor now a definition can be a set of source term, context term,
+and target term, or an example sentence."
   (let* ((source (car def))
-         (source-term (caar def))
-         (pos (cdar def)) ;; TODO: cull this
-         ;; (pos-first-word (car (split-string pos " ")))
+         (source-term-untrimmed (or (plist-get source :from) ; new term
+                                    (plist-get source :other))) ; repeat term
+         (source-term (wr--cull-double-spaces
+                       (string-trim source-term-untrimmed)))
+         (source-pos (plist-get source :pos))
          (context (cadr def))
-         (context-term (car context))
+         (context-term-untrimmed (plist-get context :other))
+         (context-term (when context-term-untrimmed
+                         (wr--cull-double-spaces
+                          (wr--cull-single-spaces-in-brackets
+                           (wr--cull-space-between-brackets
+                            (string-trim context-term-untrimmed))))))
          (target (caddr def))
-         ;; (target-pos-cull (wr-replace-pos-in-def target))
-         (target-term (car target)))
-    (setq wr-pos pos)
-    (insert
-     (concat
-      (when source-term
-        (propertize source-term
-                    'face '((t :inherit success :weight bold))))
-      " "
-      (propertize (or (wr-get-pos-from-entry source)
-                      "")
-                  'face font-lock-comment-face)
-      " "
-      context-term
-      "\n"
-      ;; (wr-print-source-or-target-str-rest (cdr source))
-      " " ;; "\n"
-      ;; pos-first-word
-      ;; (propertize
-      ;; 'face font-lock-comment-face)
-      "\n           "
-      (propertize "--> "
-                  'face font-lock-comment-face)
-      (when target-term
-        (propertize target-term
-                    'face '((t :inherit success :weight bold))))
-      " "
-      (propertize (or (wr-get-pos-from-entry target)
-                      "")
-                  'face font-lock-comment-face)
-      " "
-      ;; (wr-print-source-or-target-str-rest (cdr target)) ;-term
-      "\n"))))
-
-(defun wr-print-source-or-target-str-rest (str-list)
-  ""
-  ;; (let ((str-list (wr-abbrev-all-pos-in-def str-list wr-all-pos-abbrev-types)))
-    (concat
-     (propertize
-      (mapconcat 'identity str-list " ")
-      'face font-lock-comment-face)));)
-
-(defun wr-abbrev-all-pos-in-def (str-list pos-types)
-  ""
-  (let (new-str-list)
-    (mapcar (lambda (x)
-              (push
-               (wr-abbrev-single-pos-in-def x pos-types)
-               new-str-list))
-            str-list)
-    (reverse new-str-list)))
-
-(defun wr-abbrev-single-pos-in-def (str pos-types)
-  ""
-  (let (new-str-list)
-    (mapcar (lambda (x)
-            (if (string-prefix-p x str)
-                x
-              str))
-          pos-types)))
-
-(defvar wr-all-pos-abbrev-types
-  '("nm nom masculin"
-    "vtr verbe transitif"
-    "vi verbe intransitif"
-    "loc adj locution-adjectivale"
-    
-    "vtr transitive verb"
-    "adj adjective"
-    "n noun"
-    "vtr phrasal sep phrasal verb, transitive, separable"
-    ))
-
-(defvar wr-pos-replacement-table
-  '(("vtr verbe transitif" . "vtr verbe transitif : verbe qui s'utilise avec un complément d'objet direct (COD).")
-    ("vtr phrasal sep phrasal verb, transitive, separable" . "vtr phrasal sep phrasal verb, transitive, separable : Verb with adverb(s) or preposition(s), having special meaning, divisible--for example, \"call off\" [=cancel], \" call")))
-
-(defun wr-fix-entry (tr)
-  (let ((tds-texts
-         (wordreference--build-tds-text-list tr)))
-    (mapcar (lambda (x)
-              (concat (car x)
-                      " "
-                      (wr-get-pos-from-entry x)))
-             tds-texts)))
-
-(defun wr-fix-entries-map (table)
-  (let ((wr-trs (wordreference--get-trs table)))
-    (mapcar (lambda (x)
-              (wr-fix-entry x))
-            wr-trs)))
-
-(defun wordreference-tds-from-word-table (table)
-  (dom-by-tag table 'td))
-
-(defun wordreference-texts-from-all-tds (tds)
-  (mapcar (lambda (x)
-            (dom-texts x " |?|"))
-          wr-all-tds))
-
-(defun wordreference--get-td-text (table)
-  (dom-texts (dom-children table)))
-
-(defun wordreference--get-tr-text (tr)
-  (dom-texts (dom-children tr)))
-
-(defun wordreference--get-table-text (word-tables)
-  (dom-texts (nth 1 word-tables)))
-
-(defun wordreference--get-main-trans (tables)
-  (let ((dom-by-tag tables 'table))))
+         (target-term
+          (when target (wr--cull-double-spaces
+                        (string-trim
+                         (plist-get target :to)))))
+         (target-pos (plist-get target :pos))
+         (eg (cond ((string= (plist-get source :other) " ")
+                    (plist-get context :example))
+                   ;; process "Note" as eg, not as source-term:
+                   ((string-prefix-p "Note :" (plist-get source :other))
+                    (plist-get source :other)))))
+    (if eg
+        (insert
+         (concat "\n -- "
+                 (propertize eg
+                             'face '((t :height 0.8)))))
+      (progn
+        (insert
+         (concat
+          (when source-term
+            (if (string= source-term " ")
+                (propertize "\n\""
+                            'face font-lock-comment-face)
+              (progn
+                (concat
+                 "\n\n"
+                 (propertize
+                  source-term
+                  'face '((t :inherit warning)))))))
+          " "
+          (propertize (or source-pos
+                          "")
+                      'face font-lock-comment-face)
+          " "
+          (when (and context-term
+                     (not (string= context-term " ")))
+            (propertize context-term
+                        'face '((t :inherit font-lock-comment-face :slant italic))))
+          "\n           "
+          (propertize "--> "
+                      'face font-lock-comment-face)
+          (when target-term
+            (propertize target-term
+                        'face '((t :inherit warning))))
+          " "
+          (propertize (or target-pos
+                          "")
+                      'face font-lock-comment-face)
+          ))))))
